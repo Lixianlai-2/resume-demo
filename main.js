f1cbdb54dd1295b66b5450b0e7a8666c0a1e6f55
MarkLee <1643565650@qq.com>
2022-01-03 12:11:54 +0800

    单个css效s效果显示

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 let demo = document.querySelector("#demo");
+let style = document.querySelector("#style");
 
-let string = `你好，我是一名前端新人，
-现在开始打印CSS内容， 
-body{
-  background-color:blue;
+let string = `/*你好，我是一名前端新人，
+*现在开始打印CSS内容， 
+*body{
+*  color:blue;
+*}
+**/
+#style {
+ background-color:red;
 }
 `;
 
 let n = 0;
 let string2 = "";
+
+style.innerHTML = `
+body {
+  color:red;
+}
+`;
 
 const step = function () {
   setTimeout(() => {
@@ -20,6 +31,7 @@ const step = function () {
       string2 += string[n];
     }
     demo.innerHTML = string2;
+
     // string.length等于60，当n等于58时
     if (n < string.length - 1) {
       // n等于59，n是下标，所以string2[59]刚好打印最后一位
