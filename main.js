5c06b1ebb9bd1cece2aa082dbd17966a9d5e4d36
MarkLee <1643565650@qq.com>
2022-01-03 12:41:32 +0800

    解决缩进问题

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
 let demo = document.querySelector("#demo");
 let style = document.querySelector("#style");
 
-let string = `/*你好，我是一名前端新人，
-*现在开始打印CSS内容， 
-*body{
-*  color:blue;
-*}
-**/
-#style {
- background-color:red;
+// let string = `/*你好，我是一名前端新人，
+// *现在开始打印CSS内容，
+// *body{
+// *  color:blue;
+// *}
+// */
+// body {
+//  background-color:red;
+// }
+// `;
+
+let string = `你好，我是一名前端新人，
+现在开始打印CSS内容， 
+body{
+  color:blue;
 }
 `;
 
 let n = 0;
 let string2 = "";
 
-style.innerHTML = `
-body {
-  color:red;
-}
-`;
+// style.innerHTML = `
+// body {
+//   color:red;
+// }
+// `;
 
 const step = function () {
   setTimeout(() => {
     if (string[n] === "\n") {
       console.log("这一行有执行吗？");
       string2 += "<br>";
+    } else if (string[n] === " ") {
+      string2 += "&nbsp";
     } else {
       console.log(n);
       string2 += string[n];
     }
     demo.innerHTML = string2;
+    // style.innerHTML = string2;
 
     // string.length等于60，当n等于58时
     if (n < string.length - 1) {
