7798de348d6842bd90d1cf938d0a91bb06fb0dcd
MarkLee <1643565650@qq.com>
2022-01-03 23:34:04 +0800

    更改html中的内容

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ let style = document.querySelector("#style");
 
 let string = `/*你好，我是一名前端新人，
 *现在开始打印一个正方体，并放到右边
-*#div1{
-*  width:200px;
-* height:200px;
-*}
 */
 #div1 {
   border: 1px solid blue;
@@ -15,25 +11,20 @@ let string = `/*你好，我是一名前端新人，
   
   }
 /*
-* 接下来我把它放到右边并变为圆形
-*# div1 {
-*  border-radius:50%;
-*  box-shadow:0 0 3px rgba(0, 0, 0, 0.5);
-*  border:none;
-}
+* 接下来我把它放到中间并变为圆形
 */
 #div1 {
   border-radius:50%;
   box-shadow:0 0 3px rgba(0, 0, 0, 0.5);
   border:none;
 }
-/*接下来将其变为一左一右黑白颜色
+/*接下来将其左右变成一黑一白
 */
 #div1 {
   background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%);
 }
-
-
+/*接下来增加两个小球，通过径向梯度将它们分别设置为中间白周围黑，和中间黑周围白
+*/
 #div1::before {
   width:100px;
   height:100px;
@@ -54,8 +45,9 @@ let string = `/*你好，我是一名前端新人，
   background:#000;
   transform:translateX(-50%);
   background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 7%, rgba(0,0,0,1) 7%);
-
 }
+/*一个简单的太极图完成啦
+*/
 `;
 
 let n = 0;
@@ -84,7 +76,7 @@ const step = function () {
       console.log(string2);
       step();
     }
-  }, 1);
+  }, 30);
 };
 
 step();
